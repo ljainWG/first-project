4c3a18565b5501723efe0098673de46b8c85c268
Lakshay Jain <Lakshay.Jain@watchguard.com>
2025-01-02 14:21:25 +0530

    using signals to change user state

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal} from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -10,17 +10,20 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  public selectedUser =  DUMMY_USERS[randomIndex];  // public private protected
+  // public selectedUser =  DUMMY_USERS[randomIndex];  // public private protected
+  public selectedUser =  signal(DUMMY_USERS[randomIndex]);
 
-  get imagePath() {
-    // return '././assets/users/'+this.selectedUser.avatar;  // return the path of the image
-    return 'assets/users/'+this.selectedUser.avatar;  // return the path of the image
-  }
+  // get imagePath() {
+  //   // return '././assets/users/'+this.selectedUser.avatar;  // return the path of the image
+  //   return 'assets/users/'+this.selectedUser.avatar;  // return the path of the image
+  // }
+  imagePath = computed( () => 'assets/users/' + this.selectedUser().avatar);
   
   onClickingOnUser(){
     // console.log('User clicked');
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser =  DUMMY_USERS[randomIndex];
+    // this.selectedUser =  DUMMY_USERS[randomIndex];
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 
 }
