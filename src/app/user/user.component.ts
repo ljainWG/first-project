c6641e858a90a91c34a14e4e74a7d913ae0fc3a6
Lakshay Jain <Lakshay.Jain@watchguard.com>
2025-01-02 14:46:01 +0530

    added more users in list and removed signals

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal} from '@angular/core';
+import { Component, Input} from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -11,19 +11,28 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 export class UserComponent {
   // public selectedUser =  DUMMY_USERS[randomIndex];  // public private protected
-  public selectedUser =  signal(DUMMY_USERS[randomIndex]);
+  // public selectedUser =  signal(DUMMY_USERS[randomIndex]);
 
   // get imagePath() {
   //   // return '././assets/users/'+this.selectedUser.avatar;  // return the path of the image
   //   return 'assets/users/'+this.selectedUser.avatar;  // return the path of the image
   // }
-  imagePath = computed( () => 'assets/users/' + this.selectedUser().avatar);
+  // imagePath = computed( () => 'assets/users/' + this.selectedUser().avatar);
   
-  onClickingOnUser(){
-    // console.log('User clicked');
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    // this.selectedUser =  DUMMY_USERS[randomIndex];
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  // onClickingOnUser(){
+  //   // console.log('User clicked');
+  //   const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  //   // this.selectedUser =  DUMMY_USERS[randomIndex];
+  //   this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  // }
+
+  @Input( {required : true} ) avatar!: string; // @Input() makes this property avatar settable from outside
+  @Input( {required : true} ) name!: string;
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
   }
+
+  onClickingOnUser(){ }
 
 }
