4809b672ed1ec7887771a84d8be730dac9fe95b9
Lakshay Jain <Lakshay.Jain@watchguard.com>
2025-01-02 15:08:36 +0530

    removing signals

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input, computed} from '@angular/core';  // import input to accept inputs using signals to 
+import { Component, Input} from '@angular/core';  
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -10,32 +10,14 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // public selectedUser =  DUMMY_USERS[randomIndex];  // public private protected
-  // public selectedUser =  signal(DUMMY_USERS[randomIndex]);
 
-  // get imagePath() {
-  //   // return '././assets/users/'+this.selectedUser.avatar;  // return the path of the image
-  //   return 'assets/users/'+this.selectedUser.avatar;  // return the path of the image
-  // }
-  // imagePath = computed( () => 'assets/users/' + this.selectedUser().avatar);
-  
-  // onClickingOnUser(){
-  //   // console.log('User clicked');
-  //   const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-  //   // this.selectedUser =  DUMMY_USERS[randomIndex];
-  //   this.selectedUser.set(DUMMY_USERS[randomIndex]);
-  // }
 
-  // @Input( {required : true} ) avatar!: string; // @Input() makes this property avatar settable from outside
-  // @Input( {required : true} ) name!: string;
+  @Input( {required : true} ) avatar!: string;
+  @Input( {required : true} ) name!: string;
 
-  // get imagePath() {
-  //   return 'assets/users/' + this.avatar;
-  // }
-
-  avatar = input.required<string> (); // using signals to accept external values
-  name = input.required<string> ();
-  imagePath = computed( () => 'assets/users/' + this.avatar);
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
   onClickingOnUser(){ }
 
