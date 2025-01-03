e3587d2f9ab76ece7ca2c8c05f346ed6a8d2eac4
Lakshay Jain <Lakshay.Jain@watchguard.com>
2025-01-03 10:11:07 +0530

    added task component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,29 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'first-project';
   users = DUMMY_USERS;
-
+  selectedUserId: string = "u1";
+  
   onSelectUser(id: string) {
-    console.log('Selected user id: ' + id);
+    // console.log('Selected user id: ' + id);
+    this.selectedUserId = id;
   }
+
+  get selectedUser() {
+    return this.users.find(user => user.id === this.selectedUserId)!;
+  }
+
+
+
 
 }
